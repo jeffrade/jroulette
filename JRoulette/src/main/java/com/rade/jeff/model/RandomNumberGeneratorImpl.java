9e99403b9a9ea5a3d26ec40cc6ce6e6e92a05a99
jer3321 <jeffrade@gmail.com>
2013-10-27 08:16:24 -0500

    Added java.util.Random to generate random number

--- JRoulette/src/main/java/com/rade/jeff/model/RandomNumberGeneratorImpl.java
+++ JRoulette/src/main/java/com/rade/jeff/model/RandomNumberGeneratorImpl.java
@@ -1,10 +1,14 @@
 package com.rade.jeff.model;
 
+import java.util.Random;
+
 public class RandomNumberGeneratorImpl implements NumberGenerator{
+	
+	private Random random = new Random();
 
 	@Override
 	public int generateNumber(int numberOfIntegers) {
-		int randomNumber = (int) ((numberOfIntegers) * Math.random());
+		int randomNumber = random.nextInt(numberOfIntegers);
 		if(randomNumber < 0 || randomNumber >= numberOfIntegers){
 			System.out.println("ERROR! random number = " + randomNumber + ".  Should not have generated this number.");
 		}
